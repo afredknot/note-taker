8f5dba9fdb6df2ba359ad4647aeefa2a930e6fb5
Daniel Nelson <afredknot@gmail.com>
2022-11-30 18:25:23 -0500

    the notes will post on the html page now

--- public/assets/js/note.js
+++ public/assets/js/note.js
@@ -1,31 +1,31 @@
-const note = require('express').Router();
-const uuid = require('../../../helpers/uuid')
-const {
-  readFromFile,
-  readAndAppend,
-  } = require('../../../helpers/fsUtils');
-// GET Route for retrieving all the note
+// const note = require('express').Router();
+// const uuid = require('../../../helpers/uuid')
+// const {
+//   readFromFile,
+//   readAndAppend,
+//   } = require('../../../helpers/fsUtils');
+// // GET Route for retrieving all the note
 
-note.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-});
+// note.get('/', (req, res) => {
+//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// });
 
-// POST Route for a new UX/UI tip
-note.post('/', (req, res) => {
-  console.log(req.body);
+// // POST Route for a new UX/UI tip
+// note.post('/', (req, res) => {
+//   console.log(req.body);
 
-  if (req.body) {
-    const newNote = {
-      noteTitle,
-      noteText,
-      note_id: uuid(),
-    };
+//   if (req.body) {
+//     const newNote = {
+//       noteTitle,
+//       noteText,
+//       note_id: uuid(),
+//     };
 
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Tip added successfully 🚀`);
-  } else {
-    res.error('Error in adding tip');
-  }
-});
+//     readAndAppend(newNote, './db/db.json');
+//     res.json(`Tip added successfully 🚀`);
+//   } else {
+//     res.error('Error in adding tip');
+//   }
+// });
 
-module.exports = note;
+// module.exports = note;
--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require('path');
 const app = express();
 const PORT =  process.env.PORT || 3001;
 const makeNote = require('./db/db.json')
-const req = require('./public/assets/js/note')
+// const note = require('express').Router();
+const uuid = require('./helpers/uuid')
+
+const {
+  readFromFile,
+  readAndAppend,
+  } = require('./helpers/fsUtils');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +20,9 @@ app.use(express.static('public'));
 // app.get('api/notes', (req,res) => {
 //   res.json(makeNote.slice(1));
 // })
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(makeNote));
+});
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -46,10 +55,39 @@ function createNewNote(body, notesArray) {
   return newNote;
 }
 
-app.post('/api/notes', (req, res) => {
-  const newNote = createNewNote(req.body, makeNote);
-  res.json(newNote);
+// GET Route for retrieving all the note
+
+app.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+// app.get('/api/notes', (req, res) => {
+//   readFromFile('./db/db.json').then((data) => res.json(makeNote));
+// });
+
+// // POST Route for a new UX/UI tip
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  if (req.body) {
+    const newNote = {
+      noteTitle,
+      noteText,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+
+// app.post('/api/notes', (req, res) => {
+//   const newNote = createNewNote(req.body, makeNote);
+//   res.json(newNote);
+// });
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
